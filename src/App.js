c9a901707ee78a7504356c680f7802ef9c95fc58
dat <datle20497@gmail.com>
2021-03-22 21:33:33 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,59 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
-    <div className = "shop-cart">
-      <div className ="top-content">
-        <div className = "left-top-content">
-          <a href="#" className ="nav-link">Home </a>
-          <a href="#" className ="nav-link"> Shopping Cart</a>
-        </div>
-        <div className = "right-top-content">
-          <p className="quantity">3 items in the bag</p>
-        </div>
-        <div className = "black-line"></div>
+    <div class="main">
+      <header className="container">
+        <h1>Shopping Cart</h1>
+        <ul className="breadcrumb">
+          <li>Home</li>
+          <li>Shopping Cart</li>
+        </ul>
+        <span className="count">3 items in the bag</span>
+      </header>
+      <div className="container">
+        <ul className="products">
+          <li className="row">
+            <div className="col left">
+              <div className="thumbnail">
+                <a href="#">
+                  <img src="https://via.placeholder.com/200x150" alt />
+                </a>
+              </div>
+              <div className="detail">
+                <div className="name">
+                  <a href="#">PRODUCT ITEM NUMBER 1</a>
+                </div>
+                <div className="description">
+                  Description for product item number 1
+                </div>
+                <div className="price">$5.99</div>
+              </div>
+            </div>
+            <div className="col right">
+              <div className="quantity">
+                <input
+                  type="number"
+                  className="quantity"
+                  step={1}
+                  defaultValue={2}
+                />
+              </div>
+              <div className="remove">
+                <svg
+                  version="1.1"
+                  className="close"
+                  xmlns="//www.w3.org/2000/svg"
+                  xmlnsXlink="//www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 60 60"
+                  enableBackground="new 0 0 60 60"
+                  xmlSpace="preserve"
+                >
+                  <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
+                </svg>
+              </div>
+            </div>
+          </li>
+          <li className="row">
+            <div className="col left">
+              <div className="thumbnail">
+                <a href="#">
+                  <img src="https://via.placeholder.com/200x150" alt />
+                </a>
+              </div>
+              <div className="detail">
+                <div className="name">
+                  <a href="#">PRODUCT ITEM NUMBER 2</a>
+                </div>
+                <div className="description">
+                  Description for product item number 2
+                </div>
+                <div className="price">$9.99</div>
+              </div>
+            </div>
+            <div className="col right">
+              <div className="quantity">
+                <input
+                  type="number"
+                  className="quantity"
+                  step={1}
+                  defaultValue={1}
+                />
+              </div>
+              <div className="remove">
+                <svg
+                  version="1.1"
+                  className="close"
+                  xmlns="//www.w3.org/2000/svg"
+                  xmlnsXlink="//www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 60 60"
+                  enableBackground="new 0 0 60 60"
+                  xmlSpace="preserve"
+                >
+                  <polygon points="38.936,23.561 36.814,21.439 30.562,27.691 24.311,21.439 22.189,23.561 28.441,29.812 22.189,36.064 24.311,38.186 30.562,31.934 36.814,38.186 38.936,36.064 32.684,29.812" />
+                </svg>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
-      <div className = "main-content">
-        <div className="row">
-          <div className = "cart-item"></div>
-          <div className = "cart-item"></div>
+      <div className="container">
+        <div className="promotion">
+          <label htmlFor="promo-code">Have A Promo Code?</label>
+          <input type="text" id="promo-code" /> <button type="button" />
         </div>
-        <div className = "right-content"></div>
-      </div>
-      <div className = "bottom-content">
-        <div className="left-bottom-content">
-          <p className = "promo-title">Have A Promo Code?</p>
-          <input className = "promo-field"></input>
+        <div className="summary">
+          <ul>
+            <li>
+              Subtotal <span>$21.97</span>
+            </li>
+            <li>
+              Tax <span>$5.00</span>
+            </li>
+            <li className="total">
+              Total <span>$26.97</span>
+            </li>
+          </ul>
         </div>
-        <div className="right-bottom-content">
-          
+        <div className="checkout">
+          <button type="button">Check Out</button>
         </div>
       </div>
     </div>
-    
-
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
 export default App;
-
-
